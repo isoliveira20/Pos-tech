1dfb878ef0140c8cd50d37818d7dcd2b7f0a8fc5
isoliveira20 <izabeladesouzaoliveira@gmail.com>
2025-06-08 18:59:36 -0300

    Update bag_of_words.py

--- PNL/Processamento Linguagem Natural/bag_of_words.py
+++ PNL/Processamento Linguagem Natural/bag_of_words.py
@@ -5,3 +5,5 @@ avaliacoes.head()
 
 avaliacoes = avaliacoes.drop(["original_index", "review_text_processed", "review_text_tokenized", "rating", "kfold_polarity", "kfold_rating"], axis=1)
 avaliacoes.head()
+
+avaliacoes.dropna(inplace=True, axis=0)

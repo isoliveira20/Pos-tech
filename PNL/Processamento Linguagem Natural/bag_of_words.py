a960c442bf50dfead0a946dcc6398986756dc3fe
isoliveira20 <izabeladesouzaoliveira@gmail.com>
2025-06-08 18:41:42 -0300

    Create bag_of_words.py

---
+++ PNL/Processamento Linguagem Natural/bag_of_words.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+avaliacoes = pd.read_csv("b2w.csv")
+avaliacoes.head()
+
+avaliacoes = avaliacoes.drop(["original_index", "review_text_processed", "review_text_tokenized", "rating", "kfold_polarity", "kfold_rating"], axis=1)
+avaliacoes.head()
